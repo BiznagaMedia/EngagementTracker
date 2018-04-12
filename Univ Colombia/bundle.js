6dd4915d20db0f70dbf9f0e6558faf8dfce33484
beatrizfuentes <36053121+beatrizfuentes@users.noreply.github.com>
2018-04-12 09:40:58 -0400

    Add files via upload

---
+++ Univ Colombia/bundle.js
@@ -0,0 +1,303 @@
+var Adidas1Video = function (){
+
+    var html = '<div class="bzm_wrapper carousel-wrapper size_300x250"><a href="http://www.adidas.pe/heretocreate?cm_mmc=AdiDisplay_PR-_-originals-_-Admotion_320x480-ROS-_-Other-_-Richmedia-_-dv:Brand-_-cn:Sport18_March-_-pc:originals&cm_mmc1=PE&cm_mmc2=adidas-LATAM-Brand-Display-originals-Prospecting-PE-Sport18_March-Other-1803" style="display: block;" target="block"><div class="carousel-container"><div style="text-align:center;"> <iframe width="290" height="176" src="https://www.youtube.com/embed/ZCHqqnrQzQA?rel=0&modestbranding=1&autoplay=1&loop=1&playlist=ZCHqqnrQzQA&controls=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div></div><div class="bzm_text"> <span class="bzm_text_title bzm_text_bold" style="color:#000;">Descubre la Creatividad</span> <span class="bzm_text_title bzm_text_medium" style="display:block;color:#000;width: 100%;"><i>Reconoce a los atletas por su creatividad, no su género.</i></span></div><div style="bottom:0px;height: 18px;text-align:left;margin-top:4px;"> <img src="https://i.imgur.com/vT5WdzO.png" style="height:30px"><div id="botones-para-compartir" style="display:inline;float:left;margin-left:36px;margin-top:-20px;"> <span> <a class="shared" href="https://www.facebook.com/sharer/sharer.php?u=http://www.adidas.pe/heretocreate?cm_mmc=AdiDisplay_PR-_-originals-_-Admotion_320x480-ROS-_-Other-_-Richmedia-_-dv:Brand-_-cn:Sport18_March-_-pc:originals&cm_mmc1=PE&cm_mmc2=adidas-LATAM-Brand-Display-originals-Prospecting-PE-Sport18_March-Other-1803 " target="block"> <img alt="compartir en facebook" class="facebooki" height="16" src="http://4.bp.blogspot.com/-2W3Fr7nS-T0/Uh1HrkLe5wI/AAAAAAAAAiQ/0-_aC8Yici4/s1600/facebook.png" title="compartir en facebook" width="16" /></a><a class="shared" href="https://plus.google.com/share?url=http://www.adidas.pe/heretocreate?cm_mmc=AdiDisplay_PR-_-originals-_-Admotion_320x480-ROS-_-Other-_-Richmedia-_-dv:Brand-_-cn:Sport18_March-_-pc:originals&cm_mmc1=PE&cm_mmc2=adidas-LATAM-Brand-Display-originals-Prospecting-PE-Sport18_March-Other-1803 " target="block"> <img alt="compartir en google+" class="googlei" height="16" src="http://3.bp.blogspot.com/-Ioy5JWv_nwk/Uh1HlxE8urI/AAAAAAAAAhc/q_uUabDAhsU/s1600/Google%252B%2Balt.png" title="compartir en google+" width="16" /></a><a class="shared" href="https://twitter.com/home?status=http://www.adidas.pe/heretocreate?cm_mmc=AdiDisplay_PR-_-originals-_-Admotion_320x480-ROS-_-Other-_-Richmedia-_-dv:Brand-_-cn:Sport18_March-_-pc:originals&cm_mmc1=PE&cm_mmc2=adidas-LATAM-Brand-Display-originals-Prospecting-PE-Sport18_March-Other-1803 " target="block"> <img alt="compartir en twitter" class="twitteri" height="16" src="http://3.bp.blogspot.com/-TgonF6vzJs0/Uh1Hmxqk8aI/AAAAAAAAAiA/MdT4pRWKRbI/s1600/twitter.png" title="compartir en twitter" width="16" /></a> </span></div><div class="logo"> <a href="http://www.adidas.pe/heretocreate?cm_mmc=AdiDisplay_PR-_-originals-_-Admotion_320x480-ROS-_-Other-_-Richmedia-_-dv:Brand-_-cn:Sport18_March-_-pc:originals&cm_mmc1=PE&cm_mmc2=adidas-LATAM-Brand-Display-originals-Prospecting-PE-Sport18_March-Other-1803 " style="line-height: 31px;" target="_blank"><span style="font-size: 11.5px;font-weight:800">Más Información</span></a></div></div> </a></div>';
+
+    var css = '.Adidas1Video p{margin:0px}.Adidas1Video .bg.cover-1{background-size:cover}.Adidas1Video .btn{margin-top:0;padding-left:0}.Adidas1Video a,.Adidas1Video a:hover{text-decoration:none;color:#fff}.Adidas1Video .bzm_wrapper{position:relative;overflow:hidden;color:#000;font-family:Lato,sans-serif}.Adidas1Video .bzm_wrapper.size_300x250{width:300px;height:250px;background-color:#FFF}.Adidas1Video .bzm_cover,.Adidas1Video .carousel-wrapper{position:relative;width:100%}.Adidas1Video .bzm_cover{height:150px}.Adidas1Video .bzm_cover.bzm_pos_tl{top:3%;left:3%}.Adidas1Video .bzm_pos_tr{top:3%;right:3%}.Adidas1Video .bzm_pos_bl{bottom:3%;left:3%}.Adidas1Video .bzm_pos_br{bottom:3%;right:3%}.Adidas1Video .bzm_pos_bm{bottom:3%;left:calc(100% - 70%)}.Adidas1Video .bzm_text{padding:6px 0 0 .3em;line-height:1.1;color:#000}.Adidas1Video .bzm_text_title{font-size:14px;color:#000}.Adidas1Video .bzm_text_light{font-size:10px;font-weight:0}.Adidas1Video .bzm_text_medium{font-size:15px;font-weight:500;line-height:1.4}.Adidas1Video .bzm_text_bold{font-size:18px;font-weight:700;line-height:1}.Adidas1Video .bzm_text_small{font-size:10px;line-height:1}.Adidas1Video .carousel-wrapper{height:auto;margin:0 auto}.Adidas1Video .carousel-item1{position:absolute;opacity:0;transition:all .5s ease-in-out}.Adidas1Video .bzm_button{position:absolute;bottom:7px;left:3%;text-transform:uppercase;background-color:#6cbd44;color:#fff;border-radius:50%;padding:4px;text-align:center;font-size:90%;cursor:pointer}.Adidas1Video .bzm_button:hover{background-color:#579e38}.Adidas1Video .carousel-container{width:300px;height:150px;overflow-y:hidden}.Adidas1Video #botones-para-compartir{text-align:right}.likedini:hover,.facebooki:hover,.twitteri:hover,.googlei:hover,.pinteresti:hover{border-radius:25px 25px 25px 25px;transition:all .3s ease-out;box-shadow:0px 0px 5px #000;-moz-transition:all .5s;-webkit-transition:all .5s;-o-transition:all .5s}.likedini,.facebooki,.twitteri,.googlei,.pinteresti{transition:all .3s ease-out;-moz-transition:all .5s;-webkit-transition:all .5s;-o-transition:all .5s}.Adidas1Video .logo{width:41%;z-index:2;background-color:#0d0d0c;border-radius:4px;float:right;height:30px;text-align:center;margin-top:-35px;margin-right:12px}';
+
+
+
+  var containerE =null;
+    var rrss = ['facebook', 'twitter', 'plus.google', 'whatsapp', 'pinterest', 'mailto', 'reddit', 'linkedin',
+    'tumblr', 'mail.google', 'printfriendly', 'telegram', 'skype', 'viber', 'bufferapp', 'flipboard' ];
+    var flagReadArticleTime = false;
+    var gtmYTListeners = [];
+    var porcetScroll = 95;
+    var adsName = "Video1Adidas";
+
+    //add codigo de sequimiento de eventos
+    function addPixel() {
+               
+        var img = document.createElement('img');
+        img.setAttribute('width', 1);
+        img.setAttribute('height', 1);
+        var urltracker = 'http://www5.smartadserver.com/imp?imgid=21085124&tmstp=_ADFTAGID__';
+        img.setAttribute('src', urltracker);
+        img.setAttribute('style', 'display:none');
+
+        var body = document.querySelector('body');
+        var firstDiv = document.getElementsByTagName('div')[0];
+        body.insertBefore(img, firstDiv);
+    };
+
+     // Función que hace compatibles los eventos con todos los navegadores.
+    function addEvent(el, name, fn) {
+        if (el.addEventListener) {
+        el.addEventListener(name, fn);
+        } else if (el.attachEvent) {
+        el.attachEvent('on' + name, function(evt) {
+            evt.target = evt.target || evt.srcElement;
+            // Call the event to ensure uniform 'this' handling, pass it event
+            fn.call(el, evt);
+        });
+        } else if (typeof el['on' + name] === 'undefined' || el['on' + name] === null) {
+        el['on' + name] = function(evt) {
+            evt.target = evt.target || evt.srcElement;
+            // Call the event to ensure uniform 'this' handling, pass it event
+            fn.call(el, evt);
+        };
+        }
+    }; 
+
+     function searchShare(href) {
+        
+        for (var i = 0; i < rrss.length; i++) {
+            if (href.search(rrss[i]) != -1) {
+                return rrss[i];
+            }
+        }
+        return null;
+    };
+
+    function goToLink(link, mode) {
+        return function() {
+            if (link != undefined) {
+                switch (mode) {
+                    case undefined: {
+                        window.location = link;
+                        break;
+                    }
+                    default: {
+                        window.open(link, 'block');
+                        break;
+                    }
+                }
+            }
+        }
+    };
+
+    // Function para controlar los links de redes sociales
+    function sortLink($event) {        
+        var el = $event.currentTarget;
+        var href = el.getAttribute('href');
+        var socialMediaFlag = el.classList.contains("shared");
+        var socialShareRegister = searchShare(href);        
+
+        if (href != undefined) {
+            if (socialShareRegister == null && socialMediaFlag != -1) {
+                var target = el.getAttribute('target');
+                var data = { ref: href,'AdsName': adsName };
+                
+                if (target == '_blank' || $event.ctrlKey) {
+                    // mixpanel.track("Click Out", data, false);
+                    addPixel();
+                    
+                }else {
+                    $event.preventDefault();
+                    var callback = goToLink(href);
+                    // mixpanel.track("Click Out", data, callback);
+                    addPixel();
+                    window.setTimeout(callback, 500);       
+                }
+                
+            }else {
+                switch (socialShareRegister) {
+                    case 'plus.google': {
+                        var red = "Google+";
+                        break;
+                    }
+                    case 'mailto': {
+                        var red = "Email";
+                        break;
+                    }
+                    case 'mail.google': {
+                        var red = "Gmail";
+                        break;
+                    }
+                    case 'bufferapp': {
+                        var red = "Buffer";
+                        break;
+                    }
+                    default: {
+                        var red = socialShareRegister.charAt(0).toUpperCase() + socialShareRegister.slice(1);
+                        break;
+                    }
+                }
+                var data = {
+                    "Red": red,
+                    "AdsName": adsName
+                };
+                // mixpanel.track("Share Redes Sociales", data, false);
+                addPixel();
+                
+            }
+            return;            
+        }
+    };
+
+    //VIDEO ZONE INIT
+    // añadir listener para cada reproductor
+    window.onYouTubeIframeAPIReady = (function() {
+        // mixpanel.track("PageView", {'AdsName': adsName}, false);
+        addPixel();
+        for (var e = document.querySelectorAll(containerE + " iframe"), x = e.length; x--;) {
+            if (/youtube.com\/embed/.test(e[x].src)) {
+                 gtmYTListeners.push({
+                    id: e[x].src,
+                    eventSendStart: false,
+                    eventSend25: false,
+                    eventSend50: false,
+                    eventSend75: false,
+                    eventSendEnd: false,
+                    time: 0,
+                    event: new YT.Player(e[x], {
+                        events: {
+                            onStateChange: onPlayerStateChange
+                        }
+                    })
+                });
+                YT.gtmLastAction = "p";
+            }
+        }
+    });
+
+    // detectar play, pause, end y % avanzado (cada segundo)
+    function onPlayerStateChange(e) {
+        
+        e["data"] == YT.PlayerState.PLAYING && window.setTimeout(onPlayerPercent, 1000, e["target"]);
+        if (e["data"] == YT.PlayerState.PLAYING && YT.gtmLastAction == "p") {
+            YT.gtmLastAction = "";
+        }
+        if (e["data"] == YT.PlayerState.PAUSED) {
+            YT.gtmLastAction = "p";
+        }
+    };
+
+    function extraVideoData(e) {
+
+        var video_data = e["getVideoData"]();
+        var evAction = window.location.href;
+        var evLabel = video_data.title + ' : ' + video_data.video_id;
+        return dataVideo = {
+            'Ubicacion': evAction,
+            'Nombre del Video': evLabel,
+            'AdsName': adsName
+        };
+    };
+
+    // informar del % reproducido en estos puntos: 0.00, 0.25, 0.50, 0.75, 1 es fin
+    function onPlayerPercent(e) {
+        if (e["getPlayerState"]() == YT.PlayerState.PLAYING) {
+            var t = e["getDuration"]() - e["getCurrentTime"]() <= 1.5 ? 1 : (Math.floor(e["getCurrentTime"]() / e["getDuration"]() * 4) / 4).toFixed(2);
+            var video = null;
+            for (var i = 0; i < gtmYTListeners.length; i++) {
+                var src = e.getIframe().getAttribute('src');
+                if (gtmYTListeners[i].id == e.getIframe().getAttribute('src')) {
+                    video = gtmYTListeners[i]
+                    break;
+                }
+            }
+
+            if(t == '0.00' && !video.eventSendStart) {//Inicio de la reproduccion del video
+                gtmYTListeners[0].eventSendStart = !video.eventSendStart;
+                var dataVideo = extraVideoData(e);              
+                // mixpanel.track("video_0%", dataVideo, false);
+                addPixel();
+            }
+
+            if (t == '0.25' && !video.eventSend25) {
+
+                gtmYTListeners[i].eventSend25 = !video.eventSend25;
+                var dataVideo = extraVideoData(e);                
+                // mixpanel.track("video_25%", dataVideo, false);
+                addPixel();
+            }
+
+            if (t == '0.50' && !video.eventSend50) {
+                
+                gtmYTListeners[i].eventSend50 = !video.eventSend50;
+                var dataVideo = extraVideoData(e);                
+                // mixpanel.track("video_50%", dataVideo, false);
+                addPixel(); 
+            }
+
+            if (t == '0.75' && !video.eventSend75) {
+
+                gtmYTListeners[i].eventSend75 = !video.eventSend75;
+                var dataVideo = extraVideoData(e);                
+                // mixpanel.track("video_75%", dataVideo, false);
+               addPixel();
+            }
+            
+            if (t == 1 && !video.eventSendEnd) { //Fin de ka reproduccion del video
+
+                gtmYTListeners[i].eventSendEnd = !video.eventSendEnd;
+                var dataVideo = extraVideoData(e);                
+                // mixpanel.track("video_100%", dataVideo, false);
+                 addPixel();
+            }
+            e["lastP"] = t;
+            e["lastP"] != 1 && window.setTimeout(onPlayerPercent, 1000, e);
+        }
+    };
+
+    // cargar api de youtube e insertar
+    var j = document.createElement("script"),
+	f = document.getElementsByTagName("script")[0];
+    j.src = "https://www.youtube.com/iframe_api";
+    j.async = true;
+    f.parentNode.insertBefore(j, f);
+
+    function commonVideoAction() {
+        
+        // buscar embeds flash y reemplazar por iframe
+        for (var e = document.querySelectorAll(containerE + " embed"), x = e.length; x--;) {
+            if (/youtube.com/.test(e[x].src)) {
+                e[x].parentNode.outerHTML = e[x].outerHTML.replace(/embed/g,'iframe').replace(/youtube\.com\/v/g, 'youtube.com/embed');
+            }
+        }
+
+        // activar api
+        for (var e = document.querySelectorAll(containerE + " iframe"), x = e.length; x--;)
+            if (/youtube.com\/embed/.test(e[x].src))
+                if(e[x].src.indexOf('enablejsapi=') === -1)
+                    e[x].src += (e[x].src.indexOf('?') ===-1 ? '?':'&') + 'enablejsapi=1';        
+    };
+    //VIDEO ZONE END
+    
+
+      // Function a ejecutar cuando la pagina cargue
+    function DOMContentLoaded() {
+       
+        //Detectar las etiquetas <a> que existen en el anuncio y asignarle el evento click
+        var links = document.querySelectorAll(containerE + " a"); 
+        for (var i = 0; i < links.length; i++) {
+            addEvent(links[i], 'click', sortLink);
+        }
+    }; 
+
+    return {
+        show: function (container) {
+            
+            var containerElement = document.querySelector('.'+container);
+
+            if (containerElement != null) {
+                containerE='.'+container;
+                var head = document.getElementsByTagName('head');
+                var style = document.createElement('style');  
+                var text = document.createTextNode(css);            
+                style.appendChild(text);   
+                head[0].appendChild(style);
+
+                containerElement.innerHTML = html;
+
+                window.setTimeout(DOMContentLoaded, 5000);
+                //Detectar los videos que existen en el anuncio
+                commonVideoAction(); 
+
+                var containerElement = document.querySelector(containerE + ' .bzm_wrapper');
+                addEvent(containerElement, 'scroll', scrollListener);
+            }    
+        }
+    };
+}();
+
